a8e8599f341a0ef7b6c97257b8bc98c17510d382
Jorge Matos <jisapjisap@gmail.com>
2024-12-08 12:15:06 +0100

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import clsx from "clsx";
+import gsap from "gsap";
+//import { useWindowScroll } from "react-use";
+import { useEffect, useRef, useState } from "react";
+import { TiLocationArrow } from "react-icons/ti";
+
+import Button from "./Button";
+
+const navItems = ["Nexus", "Vault", "Prologue", "About", "Contact"];
 
 const Navbar = () => {
+
+  const navContainerRef = useRef(null);
+
   return (
-    <div>Navbar</div>
+    <div
+      ref={navContainerRef}
+      className="fixed inset-x-0 top-4 z-50 h-16 border-none transition-all duration-700 sm:inset-x-6"
+    >
+      <header className="absolute top-1/2 w-full -translate-y-1/2">
+        
+      </header>
+    </div>
   )
 }
 
