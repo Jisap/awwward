43788a73e02bf88edcb6aae90f4ec80f3a390980
Jorge Matos <jisapjisap@gmail.com>
2024-12-08 11:40:56 +0100

    AnimatedTitle

--- src/components/AnimatedTitle.jsx
+++ src/components/AnimatedTitle.jsx
@@ -5,7 +5,13 @@ const AnimatedTitle = ({ title, containerClass }) => {
     <div className='mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]'>
       {title.split("<br />").map((line, index) => (
         <div key={index} className='flex-center max-w-full flex-wraps gap-2 px-10 md:gap-3'>
-          {line}
+          {line.split(" ").map((word, i) => (
+            <span 
+              key={i}
+              className="animated-word"
+              dangerouslySetInnerHTML={{ __html: word }}  
+            />
+          ))}
         </div>
       ))}
     </div>
