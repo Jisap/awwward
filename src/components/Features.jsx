5f894481764adc5488ba45d98741dbe35f4d7a80
Jorge Matos <jisapjisap@gmail.com>
2024-12-09 11:59:01 +0100

    Features

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,5 +1,29 @@
 import React from 'react'
 
+const BentoCard = ({ src, title, description, isComingSoon }) => {
+  return (
+    <div className="relative size-full">
+      <video
+        src={src}
+        loop
+        muted
+        autoPlay
+        className="absolute left-0 top-0 size-full object-cover object-center"
+      />
+      <div className="relative z-10 flex size-full flex-col justify-between p-5 text-blue-50">
+        <div>
+          <h1 className="bento-title special-font">{title}</h1>
+          {description && (
+            <p className="mt-3 max-w-64 text-xs md:text-base">{description}</p>
+          )}
+        </div>
+      </div>
+      {title}
+    </div>
+  )
+}
+
+
 const Features = () => {
   return (
     <section className="bg-black pb-52">
@@ -13,6 +37,19 @@ const Features = () => {
             array of products converge into an interconnected overlay experience
             on your world.
           </p>
+        </div>
+
+        <div className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
+          <BentoCard 
+            src="videos/feature-1.mp4"
+            title={
+              <>
+                radia<b>n</b>t
+              </>
+            }
+            description="A cross-platform metagame app, turning your activities across Web2 and Web3 games into a rewarding adventure."
+            isComingSoon
+          />
         </div>
       </div>
     </section>
